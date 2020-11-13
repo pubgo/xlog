f0af5939a5cb60247f93d5969b72613e293c54c7
白云辉 <baiyh@weipaitang.com>
2020-11-13 14:32:47 +0800

    fix: update

--- xlog.go
+++ xlog.go
@@ -1,18 +1,17 @@
 package xlog
 
 import (
-	"go.uber.org/zap"
-
 	"github.com/pubgo/xerror"
 	"github.com/pubgo/xlog/internal"
 	"github.com/pubgo/xlog/internal/log"
+	"go.uber.org/zap"
 )
 
 type XLog = internal.XLog
 
 func New(zl *zap.Logger) XLog {
 	if zl == nil {
-		panic("zap.Logger should not be nil")
+		xerror.Panic(xerror.New("zap.Logger [zl] should not be nil"))
 	}
 	return log.NewXLog().SetZapLogger(zl)
 }
