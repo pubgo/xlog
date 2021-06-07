0a3d2b335edc3ae55dbbc4e6ae7688314fd1c652
白云辉 <kooksee@163.com>
2021-06-07 17:37:07 +0800

    fix: update filter

--- xlog_grpc/grpc.go
+++ xlog_grpc/grpc.go
@@ -15,54 +15,127 @@ func Init(log xlog.Xlog) {
 }
 
 type loggerWrapper struct {
-	log xlog.Xlog
+	log           xlog.Xlog
+	printFilter   func(args ...interface{}) bool
+	printfFilter  func(format string, args ...interface{}) bool
+	printlnFilter func(args ...interface{}) bool
+}
+
+func (l *loggerWrapper) SetPrintFilter(filter func(args ...interface{}) bool) {
+	l.printFilter = filter
+}
+func (l *loggerWrapper) SetPrintfFilter(filter func(format string, args ...interface{}) bool) {
+	l.printfFilter = filter
+}
+func (l *loggerWrapper) SetPrintlnFilter(filter func(args ...interface{}) bool) {
+	l.printlnFilter = filter
+}
+
+func (l *loggerWrapper) filter(args ...interface{}) bool {
+	return l.printFilter != nil && l.printFilter(args...)
+}
+
+func (l *loggerWrapper) filterf(format string, args ...interface{}) bool {
+	return l.printfFilter != nil && l.printfFilter(format, args...)
+}
+
+func (l *loggerWrapper) filterln(args ...interface{}) bool {
+	return l.printlnFilter != nil && l.printlnFilter(args...)
 }
 
 func (l *loggerWrapper) Info(args ...interface{}) {
+	if l.filter(args) {
+		return
+	}
+
 	l.log.InfoW(func(log xlog_abc.Logger) { log.Print(args...) })
 }
 
 func (l *loggerWrapper) Infoln(args ...interface{}) {
+	if l.filterln(args) {
+		return
+	}
+
 	l.log.InfoW(func(log xlog_abc.Logger) { log.Println(args...) })
 }
 
 func (l *loggerWrapper) Infof(format string, args ...interface{}) {
+	if l.filterf(format, args...) {
+		return
+	}
+
 	l.log.InfoW(func(log xlog_abc.Logger) { log.Printf(format, args...) })
 }
 
 func (l *loggerWrapper) Warning(args ...interface{}) {
+	if l.filter(args...) {
+		return
+	}
+
 	l.log.WarnW(func(log xlog_abc.Logger) { log.Print(args...) })
 }
 
 func (l *loggerWrapper) Warningln(args ...interface{}) {
+	if l.filterln(args) {
+		return
+	}
+
 	l.log.WarnW(func(log xlog_abc.Logger) { log.Println(args...) })
 }
 
 func (l *loggerWrapper) Warningf(format string, args ...interface{}) {
+	if l.filterf(format, args...) {
+		return
+	}
+
 	l.log.WarnW(func(log xlog_abc.Logger) { log.Printf(format, args...) })
 }
 
 func (l *loggerWrapper) Error(args ...interface{}) {
+	if l.filter(args...) {
+		return
+	}
+
 	l.log.ErrorW(func(log xlog_abc.Logger) { log.Print(args...) })
 }
 
 func (l *loggerWrapper) Errorln(args ...interface{}) {
+	if l.filterln(args) {
+		return
+	}
+
 	l.log.ErrorW(func(log xlog_abc.Logger) { log.Println(args...) })
 }
 
 func (l *loggerWrapper) Errorf(format string, args ...interface{}) {
+	if l.filterf(format, args...) {
+		return
+	}
+
 	l.log.ErrorW(func(log xlog_abc.Logger) { log.Printf(format, args...) })
 }
 
 func (l *loggerWrapper) Fatal(args ...interface{}) {
+	if l.filter(args...) {
+		return
+	}
+
 	l.log.FatalW(func(log xlog_abc.Logger) { log.Print(args...) })
 }
 
 func (l *loggerWrapper) Fatalln(args ...interface{}) {
+	if l.filterln(args) {
+		return
+	}
+
 	l.log.FatalW(func(log xlog_abc.Logger) { log.Println(args...) })
 }
 
 func (l *loggerWrapper) Fatalf(format string, args ...interface{}) {
+	if l.filterf(format, args...) {
+		return
+	}
+
 	l.log.FatalW(func(log xlog_abc.Logger) { log.Printf(format, args...) })
 }
 
